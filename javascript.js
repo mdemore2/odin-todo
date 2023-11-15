d378068ef39aa9b772e2e54d6d244713b31896c1
Mark Demore <53943861+mdemore2@users.noreply.github.com>
2023-11-14 21:27:28 -0500

    Added modals, css to support, and js shells

--- javascript.js
+++ javascript.js
@@ -1,3 +1,15 @@
 function createToDo(title, descprition, project, dueDate, priority) {
+
+}
+
+function createProject(name, todoList){
+
+}
+
+function displayProject(){
+
+}
+
+function displayToDO(){
     
 }
