173fc7cddbbb57b49d6faf8af773fa9d5c2dd720
Mark Demore <53943861+mdemore2@users.noreply.github.com>
2023-11-18 16:23:07 -0500

    Basic modal functions added to javscript.js

--- javascript.js
+++ javascript.js
@@ -10,6 +10,35 @@ function displayProject(){
 
 }
 
-function displayToDO(){
+function displayToDo(){
     
 }
+
+const newToDo = document.getElementById("new-todo");
+const newProject = document.getElementById("new-project");
+
+const newToDoOpen = document.getElementById("open-new-todo");
+const newProjectOpen = document.getElementById("open-new-project");
+
+const newToDoClose = document.getElementById("close-todo");
+const newProjectClose = document.getElementById("close-project");
+
+newToDoOpen.addEventListener("click", () => {
+    newToDo.showModal();
+})
+
+newToDoClose.addEventListener("click", (e) => {
+    e.preventDefault();
+    newToDo.close();
+    //TODO handle data
+})
+
+newProjectOpen.addEventListener("click", () => {
+    newProject.showModal();
+})
+
+newProjectClose.addEventListener("click", (e) => {
+    e.preventDefault();
+    newProject.close();
+    //TODO handle data
+})
